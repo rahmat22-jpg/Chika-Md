b44572f6ad31c8b6367543049a3be9d86d7d2d64
Rahmat Hidayat <66947554+rahmat22-jpg@users.noreply.github.com>
2024-04-09 08:23:32 +0700

    RHDYT-BOT-SETTING.js

--- settings.js
+++ settings.js
@@ -22,27 +22,24 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6285814529267','6285814529267','6285814529267'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6285814529267'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6285814529267' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Rahmat Hidayat' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'RHDYT-BOT' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'RAHMAT HIDAYAT' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
+global.mygc = 'https://chat.whatsapp.com/J2rXee1sMb9KTQOq8aLRCB'
+global.ig = 'https://instagram.com/rhhdytt_'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© RHDYT-BOT' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'RAHMAT HIDAYAT' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
